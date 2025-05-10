e0ae69fc018f80e7d929fd7f70098d7291dbce61
Pop Raluca <raluca.pop03@e-uvt.ro>
2025-05-10 15:38:47 +0300

    fix1

--- src/main/java/proiectLicenta/DentHelp/DentHelpApplication.java
+++ src/main/java/proiectLicenta/DentHelp/DentHelpApplication.java
@@ -3,6 +3,7 @@ package proiectLicenta.DentHelp;
 import io.github.cdimascio.dotenv.Dotenv;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 import org.springframework.scheduling.annotation.EnableScheduling;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -12,7 +13,7 @@ import org.springframework.web.bind.annotation.RestController;
 @SpringBootApplication
 @EnableScheduling
 
-public class DentHelpApplication {
+public class DentHelpApplication extends SpringBootServletInitializer {
 
 	public static void main(String[] args) {
 
